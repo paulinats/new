aedf76f6ccbb3c7c1231b6c991414ea1791edabc
paulinats <71715041+paulinats@users.noreply.github.com>
2021-07-12 15:43:53 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-esfdrwrewr
+
     <App />
   </StrictMode>,
   rootElementkasjdkasd
